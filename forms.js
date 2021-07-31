edcd1ee3af035d2d3ed456c909e8d35d1cbf2d9d
bhanuteja <bhanutejasangars@gmail.com>
2021-07-31 17:41:13 +0530

    cellphone field validation and city field updated

--- forms.js
+++ forms.js
@@ -1,26 +1,57 @@
 
+function y() {   
+var firstname=document.getElementById("firstname").value;
+var email=document.getElementById("email").value;
+var add1=document.getElementById("add1").value;
+var City=document.getElementById("City").value;
+var country=document.getElementById("country").value;
+var Cellphone=document.getElementById("Cellphone").value;
+var popup=[];
 
-
-var a=document.getElementById("firstname").value;
-var b=document.getElementById("email").value;
-var c=document.getElementById("add1").value;
-var d=document.getElementById("City").value;
-var e=document.getElementById("country").value
-
-function x(){
-   if( a==""){
+console.log(Cellphone.length)
+  if( firstname==""){
        document.getElementById("firstname").style.borderColor = "red";
-   }  
-  if( b==""){
+   }  else{
+       document.getElementById("firstname").style.borderColor ="green";
+   }
+  if( email==""){
        document.getElementById("email").style.borderColor = "red";
-  }
-  if(c==""){
-       document.getElementById("add1").style.borderColor ="red";
-   }
-   if( d==""){
-       document.getElementById("City").style.borderColor = "red";
-   }
-   if(e==""){
-       document.getElementById("country").style.borderColor ="red";
-   }
+  }else{
+    document.getElementById("email").style.borderColor ="green";
 }
+  if(add1==""){
+       document.getElementById("add1").style.borderColor ="red";
+   }else{
+    document.getElementById("add1").style.borderColor ="green";
+}
+   if( City==""){
+       document.getElementById("City").style.borderColor = "red";
+   }else{
+    document.getElementById("City").style.borderColor ="green";
+}
+   if(country==""){
+       document.getElementById("country").style.borderColor ="red";
+   }else{
+    document.getElementById("country").style.borderColor ="green";
+}
+   if(Cellphone==""){
+       document.getElementById("Cellphone").style.borderColor="red";
+   }else{
+    document.getElementById("Cellphone").style.borderColor ="green";
+}
+  if(Cellphone.length<6){
+      popup.push('phone number cant be less than 6 digits')
+      document.getElementById("Warning").innerText=popup.join(',')
+      document.getElementById("Warning").style.color="red"
+      document.getElementById("Cellphone").style.borderColor ="red";
+}
+  }
+  if(Cellphone.length>13){
+    popup.push('phone number cant be less than 13 digits')
+    document.getElementById("Warning").innerText=popup.join(',')
+    document.getElementById("Warning").style.color="red"
+    document.getElementById("Cellphone").style.borderColor ="red";
+}
+
+
+
