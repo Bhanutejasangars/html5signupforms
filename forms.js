b4d8d4386d775cf7b1376967669a17abe8251510
bhanuteja <bhanutejasangars@gmail.com>
2021-08-04 22:28:51 +0530

    "changed function name"

--- forms.js
+++ forms.js
@@ -8,7 +8,7 @@ var country=document.getElementById("country").value;
 var Cellphone=document.getElementById("Cellphone").value;
 var popup=[];
 
-console.log(Cellphone.length)
+
   if( firstname==""){
        document.getElementById("firstname").style.borderColor = "red";
    }  else{
@@ -53,5 +53,29 @@ console.log(Cellphone.length)
     document.getElementById("Cellphone").style.borderColor ="red";
 }
 
+function dropdown(listindex){
+    document.getElementById("City").options.length=0;
+    document.write('<select id="City"><option id="">select city</option></select>') 
+    switch(listindex){
+        case "Telangana":
+    document.getElementById("City").options[0]=new Option("please select city","");
+    document.getElementById("City").options[1]=new Option("MANCHERIAL","MANCHERIAL");
+    document.getElementById("City").options[2]=new Option("HYDERABAD","HYDERABAD");
+    document.getElementById("City").options[3]=new Option("NIZAMBAD","NIZAMBAD");
+    document.getElementById("City").options[4]=new Option("ADILABAD","ADILABAD");
+       break;
+       case "Tamilnadu":
+    document.getElementById("City").options[0]=new Option("please select city","");
+    document.getElementById("City").options[1]=new Option("CHENNAI","CHENNAI");
+    document.getElementById("City").options[2]=new Option("COIAMBATORE","COIAMBATORE");
+    break;
+    case "Maharastra":
+        document.getElementById("City").options[0]=new Option("please select city","");
+        document.getElementById("City").options[0]=new Option("MUMBAI","MUMBAI");
+        document.getElementById("City").options[0]=new Option("PUNE","PUNE");
+        break;
+    }
+    return true;
+}
 
-
+ 
